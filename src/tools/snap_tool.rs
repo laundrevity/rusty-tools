2ee5e872a3505274f38bf174ff611791d182a54a
laundrevity <mahany25@gmail.com>
2023-12-07 15:26:31 -0500

    recursive snapshot

--- src/tools/snap_tool.rs
+++ src/tools/snap_tool.rs
@@ -1,5 +1,6 @@
 use crate::{traits::Tool, types::AppError};
 use async_trait::async_trait;
+use async_recursion::async_recursion;
 use serde_json::{json, Value as JsonValue, Result as JsonResult};
 use serde::de::Error as DeError;
 use std::path::Path;
@@ -27,24 +28,18 @@ impl Tool for SnapTool {
     }
 }
 
-// Function to create the snapshot
+// Function to recursively create the snapshot
 async fn create_project_snapshot() -> Result<String, AppError> {
+    let root_path = Path::new("src");
     let mut snapshot = String::new();
 
-    // Read the Cargo.toml file
+    // Include Cargo.toml at the root of the snapshot
     snapshot.push_str("File: Cargo.toml\n");
     snapshot.push_str(&read_file_contents("Cargo.toml").await?);
     snapshot.push_str("\n\n");
 
-    // Read all .rs files in src
-    for entry in fs::read_dir("src")? {
-        let path = entry?.path();
-        if path.is_file() && path.extension().and_then(std::ffi::OsStr::to_str) == Some("rs") {
-            snapshot.push_str(&format!("File: {}\n", path.display()));
-            snapshot.push_str(&read_file_contents(&path).await?);
-            snapshot.push_str("\n\n");
-        }
-    }
+    // Start the recursion from the root_path, which is "src"
+    read_directory_contents(&root_path, &mut snapshot).await?;
 
     // Write snapshot to state.txt
     fs::write("state.txt", &snapshot)
@@ -53,6 +48,31 @@ async fn create_project_snapshot() -> Result<String, AppError> {
     // Return the created snapshot
     Ok(snapshot)
 }
+
+// Helper to read the contents of a directory recursively
+#[async_recursion]
+async fn read_directory_contents<P: AsRef<Path> + Send>(dir_path: P, snapshot: &mut String) -> Result<(), AppError> {
+    let dir_path = dir_path.as_ref();
+
+    // Iterate over the directory
+    for entry in fs::read_dir(dir_path).map_err(AppError::IOError)? {
+        let entry = entry.map_err(AppError::IOError)?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            // Recursively call this function for nested directories
+            read_directory_contents(&path, snapshot).await?;
+        } else if path.is_file() && path.extension().and_then(std::ffi::OsStr::to_str) == Some("rs") {
+            // Add the contents of Rust source files to the snapshot
+            snapshot.push_str(&format!("File: {}\n", path.strip_prefix("src/").unwrap_or(&path).display()));
+            snapshot.push_str(&read_file_contents(&path).await?);
+            snapshot.push_str("\n\n");
+        }
+    }
+
+    Ok(())
+}
+
 
 // Helper to read file contents
 async fn read_file_contents<P: AsRef<Path>>(path: P) -> JsonResult<String> {
@@ -63,7 +83,6 @@ async fn read_file_contents<P: AsRef<Path>>(path: P) -> JsonResult<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
     #[tokio::test]
     async fn test_get_snapshot() {
         // This test assumes that there is a Cargo.toml file and at least one .rs file in the src directory.
